89ff3bbe74796707109e6baed3182b3c61b34433
semblant <robynhumphreys7_4@hotmail.com>
2024-10-31 21:37:11 -0600

    Add server configuration

---
+++ express_server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+const PORT = 8080; // default port 8080
+
+const urlDatabase = {
+  b2xVn2: "http://www.lighthouselabs.ca",
+  "9sm5xK": "http://www.google.com",
+};
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+})
