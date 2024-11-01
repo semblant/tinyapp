211e1df09f995ca6e8bd025774a46c4dfc637f13
semblant <robynhumphreys7_4@hotmail.com>
2024-10-31 23:17:28 -0600

    Add post method to add new URl to db

--- express_server.js
+++ express_server.js
@@ -4,15 +4,31 @@ const PORT = 8080; // default port 8080
 
 app.set('view engine', 'ejs'); // set ejs as templating engine
 
+app.use(express.urlencoded({ extended: true }));
+
 const urlDatabase = {
   b2xVn2: "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com",
 };
 
 
+/**
+ * Function generates a random URL ID
+ *
+ * @returns {string} - the random ID string
+ */
+const generateRandomID = () => {
+  return Math.random().toString(36).substring(6);
+};
+
 // Root route, sends a plain text response "Hello World!" to the client
 app.get('/', (req, res) => {
   res.send('Hello World!');
+});
+
+// Route that responds with HTML-formatted "Hello World!" content
+app.get('/hello', (req, res) => {
+  res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
 
 // Route to return the urlDatabase as a JSON object
@@ -26,15 +42,22 @@ app.get('/urls', (req, res) => {
   res.render('urls_index', templateVars);
 });
 
+// Route to create a new URL, renders an HTML template form to submit a new URL
+app.get('/urls/new', (req, res) => {
+  res.render('urls_new')
+});
+
+app.post('/urls', (req, res) => {
+  const newId = generateRandomID();
+  const newLongURL = req.body.longURL;
+  urlDatabase[newId] = newLongURL;
+  res.redirect(`${req.body.longURL}`)
+});
+
 // Dynamic route to display a specific URL's details based on the id provided
 app.get('/urls/:id', (req, res) => {
   const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id] };
   res.render('urls_show', templateVars);
-});
-
-// Route that responds with HTML-formatted "Hello World!" content
-app.get('/hello', (req, res) => {
-  res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
 
 app.listen(PORT, () => {
