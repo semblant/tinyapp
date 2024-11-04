c9bb29c701242c7ac1f3b7c4781f910b392dee91
semblant <robynhumphreys7_4@hotmail.com>
2024-11-03 18:53:42 -0700

    Add feature to display username when user is logged in

--- express_server.js
+++ express_server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
 const app = express();
 const PORT = 8080; // default port 8080
 
@@ -7,6 +8,7 @@ app.set('view engine', 'ejs'); // set ejs as templating engine
 
 app.use(express.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+app.use(cookieParser());
 
 const urlDatabase = {
   b2xVn2: "http://www.lighthouselabs.ca",
@@ -22,14 +24,9 @@ const generateRandomID = () => {
   return Math.random().toString(36).substring(6);
 };
 
-// Root route, sends a plain text response "Hello World!" to the client
+// Root route, redirects to /urls page
 app.get('/', (req, res) => {
-  res.send('Hello World!');
-});
-
-// Route that responds with HTML-formatted "Hello World!" content
-app.get('/hello', (req, res) => {
-  res.send("<html><body>Hello <b>World</b></body></html>\n");
+  res.redirect('/urls');
 });
 
 // Route to return the urlDatabase as a JSON object
@@ -39,12 +36,13 @@ app.get('/urls.json', (req, res) => {
 
 // Route to display a list of URLs, renders an HTML template with url data
 app.get('/urls', (req, res) => {
-  const templateVars = { urls: urlDatabase };
+  const templateVars = { urls: urlDatabase, username: req.cookies['username'] };
   res.render('urls_index', templateVars);
 });
 
 // Route to create a new URL, renders an HTML template form to submit a new URL
 app.get('/urls/new', (req, res) => {
+  const templateVars = { username: req.cookies['username'] }
   res.render('urls_new')
 });
 
@@ -56,7 +54,7 @@ app.post('/urls', (req, res) => {
   res.redirect(`/urls/${newId}`)
 });
 
-// Route to login page, redirect to /urls
+// Route to post username to login page then redirect to /urls
 app.post('/login', (req, res) => {
   const username = req.body.username;
   res.cookie('username', username);
@@ -71,7 +69,7 @@ app.get('/u/:id', (req, res) => {
 
 // Dynamic route to display a specific URL's details based on the id provided
 app.get('/urls/:id', (req, res) => {
-  const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id] };
+  const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id], username: req.cookies['username'] };
   res.render('urls_show', templateVars);
 });
 
@@ -87,7 +85,7 @@ app.post('/urls/:id', (req, res) => {
   const currentID = req.body.currentID; // Grab data from hidden form named 'currentID'
   const updatedURL = req.body.newURL; // Grab data from form named 'newURL'
   urlDatabase[currentID] = updatedURL; // update db
-  const templateVars = { id: currentID, longURL: updatedURL };
+  const templateVars = { id: currentID, longURL: updatedURL, username: req.cookies['username'] };
   res.render('urls_show', templateVars);
 });
 
