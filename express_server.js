009cb368bba0529dcd6f6264bff6ad6d242de097
semblant <robynhumphreys7_4@hotmail.com>
2024-11-06 19:59:36 -0700

    Update Basic Permission Features

--- express_server.js
+++ express_server.js
@@ -1,21 +1,33 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
+const morgan = require('morgan');
+
+// Constants
 const app = express();
 const PORT = 8080; // default port 8080
-
-app.set('view engine', 'ejs'); // set ejs as templating engine
-
-app.use(express.urlencoded({ extended: true }));
-app.use(bodyParser.json());
-app.use(cookieParser());
-
 const urlDatabase = {
   b2xVn2: "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com",
 };
 
 const userDatabase = {};
+
+
+
+
+// View Engine
+app.set('view engine', 'ejs'); // set ejs as templating engine
+
+
+// Middleware
+app.use(express.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(cookieParser());
+app.use(morgan('dev'));
+
+
+// HTTP METHOD HANDLERS
 
 /**
  * Function generates a random URL ID
@@ -105,32 +117,47 @@ app.get('/urls', (req, res) => {
 // Route to create a new URL, renders an HTML template form to submit a new URL
 app.get('/urls/new', (req, res) => {
   const templateVars = { user: userDatabase[req.cookies.user_id] };
+  if (userLookup(req.cookies.user_id) === null ) return res.redirect('/login')
   res.render('urls_new', templateVars);
 });
 
 // Route to post a new URL and store the data in the database, then redirect to the specific URL for the ID
 app.post('/urls', (req, res) => {
+  // Handle case where user is not logged in
+  if (userLookup(req.body) === null) return res.status(403).send('Must be registered and logged in to manipulate URLs.')
+
   const newId = generateRandomID();
   const newLongURL = req.body.longURL;
   urlDatabase[newId] = newLongURL;
   res.redirect(`/urls/${newId}`);
 });
 
-
 // Route to redirect any shortURl (/u/:id) to its longURL
 app.get('/u/:id', (req, res) => {
   const longURL = urlDatabase[req.params.id];
+
+  if (!longURL) return res.status(404).send('That URL doesn\'t exist.');
+
   res.redirect(`${longURL}`);
 });
 
 // Dynamic route to display a specific URL's details based on the id provided
 app.get('/urls/:id', (req, res) => {
   const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id], user: userDatabase[req.cookies.user_id] };
+
+  // Handle case where user is not logged in
+  if (userLookup(templateVars.user) === null) return res.status(403).send('Must be registered and logged in to manipulate URLs.')
+
+
   res.render('urls_show', templateVars);
 });
 
 // Dynamic route to delete a URL from the database and redirect to the /urls page
 app.post('/urls/:id/delete', (req, res) => {
+  // Handle case where user is not logged in
+  if (userLookup(req.body.user_id) === null) return res.status(403).send('Must be registered and logged in to manipulate URLs.')
+
+  // Case where user is logged in
   const urlToDelete = req.params.id;
   delete urlDatabase[urlToDelete];
   res.redirect('/urls');
